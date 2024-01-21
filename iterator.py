f63d8f6f7d5e7c136fe39a2b12079566bc396eff
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-21 22:32:44 +0530

    Add files via upload

---
+++ iterator.py
@@ -0,0 +1,23 @@
+# mytuple = ("apple", "banana", "cherry")
+# myit = iter(mytuple)
+
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+
+
+# mystr = "banana"
+# myit = iter(mystr)
+
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
